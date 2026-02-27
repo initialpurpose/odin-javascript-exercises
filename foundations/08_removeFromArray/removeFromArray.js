f20a9ded22370e8f26b8dae3148f8a92d368ef27
initialpurpose <51514823+initialpurpose@users.noreply.github.com>
2026-02-27 15:36:24 +0000

    removeFromArray.js: update function to handle multiple params
    Refactor `removeFromArray` to accept an array and multiple parameters. Implement logic to remove all occurrences of each parameter from the array.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...params) {
+  for (let i = 0; i < params.length; i++) {
+    let repeat = true;
+
+    while (repeat) {
+      const index = array.indexOf(params[i]);
+      if (index > -1) array.splice(index, 1);
+
+      if (array.indexOf(params[i]) === -1) {
+        repeat = false;
+      }
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
