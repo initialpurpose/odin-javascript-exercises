bad80bde0ed70288f4656cb2c4a7ab3ff95ee608
initialpurpose <51514823+initialpurpose@users.noreply.github.com>
2026-02-27 16:32:03 +0000

    tempConversion.js: add temperature conversion logic
    Update `convertToCelsius` and `convertToFahrenheit` to accept a `temp` parameter and return calculated values. Round results to 1 decimal place when necessary and ensure outputs are numbers.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let result = (temp - 32) * 5/9;
+    if (result % 1 !== 0) result = result.toFixed(1);
+
+    return Number(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    let result = (temp * (9/5)) + 32;
+    if (result % 1 !== 0) result = result.toFixed(1);
+
+    return Number(result);
 };
 
 // Do not edit below this line
