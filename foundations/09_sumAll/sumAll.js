a0e1fa0b8982555d15a485fb25dd0171349f6a37
initialpurpose <51514823+initialpurpose@users.noreply.github.com>
2026-02-27 15:58:33 +0000

    sumAll.js: add range summation with error handling
    Introduce parameters to `sumAll` for calculating the sum of all integers in a given range. Add logic to ensure inputs are positive integers and handle invalid inputs by returning "ERROR".

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumNums = 0;
 
+    if (num1 > num2) {
+        let storeNum = num1;
+
+        // Set num1 to smaller number
+        num1 = num2;
+        num2 = storeNum;
+    }
+
+    if ((num1 > 0 && num2 > 0) &&
+        Number.isInteger(num1) &&
+        Number.isInteger(num2)) {
+        for (let i = num1; i >= num1 && i <= num2; i++) {
+            sumNums += i;
+        }
+
+        return sumNums;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
